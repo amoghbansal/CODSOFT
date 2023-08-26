12ccd868ba2b6062be5996bdd208dd45599ac64f
Amogh Bansal <bansalamogh0@gmail.com>
2023-08-27 00:57:59 +0530

    Add files via upload

--- Task 1/main.py
+++ Task 1/main.py
@@ -1,20 +1,7 @@
-# Step 1: Data Preparation
-# You'll need a dataset containing movie plot summaries and their corresponding genres. You can obtain such a dataset from various sources, like IMDb or Kaggle. Make sure the data is well-formatted and contains both text (plot summary) and target labels (genres).
-# 
-# Step 2: Data Preprocessing
-# Before building the model, we need to preprocess the text data. This includes removing punctuation, converting text to lowercase, and removing stop words. We will also tokenize the text to convert it into a format suitable for machine learning algorithms.
-# 
-# Step 3: Feature Extraction using TF-IDF
-# We will use TF-IDF (Term Frequency-Inverse Document Frequency) to convert text data into numerical features. TF-IDF represents the importance of a word in a document relative to a collection of documents. It helps in vectorizing the text data for machine learning.
-# 
-# Step 4: Model Building
-# We'll use three classifiers: Naive Bayes, Logistic Regression, and Support Vector Machines (SVM). We'll train each model on the TF-IDF transformed data and evaluate their performance.
-
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-# Step 1: Load data from text files and create DataFrames
 train_data_file = "train_data.txt"
 test_data_file = "test_data.txt"
 
@@ -29,22 +16,17 @@ def load_data(file_path):
 train_data = pd.DataFrame(load_data(train_data_file), columns=["ID", "TITLE", "GENRE", "DESCRIPTION"])
 test_data = pd.DataFrame(load_data(test_data_file), columns=["ID", "TITLE", "DESCRIPTION"])
 
-# Step 3: Feature Extraction using TF-IDF
-vectorizer = TfidfVectorizer(max_features=5000)  # Limiting to the top 5000 features
+vectorizer = TfidfVectorizer(max_features=5000)
 X_train_tfidf = vectorizer.fit_transform(train_data['DESCRIPTION'])
 X_test_tfidf = vectorizer.transform(test_data['DESCRIPTION'])
 
-# Step 4: Model Building
 y_train = train_data['GENRE']
 
-# Initialize and train a classifier
-classifier = MultinomialNB()  # You can choose any other classifier like Logistic Regression or SVM here
+classifier = MultinomialNB()
 classifier.fit(X_train_tfidf, y_train)
 
-# Step 5: Make predictions on the test data
 predictions = classifier.predict(X_test_tfidf)
 
-# Step 6: Save the predictions to a text file
 test_data['PREDICTED_GENRE'] = predictions
 
 output_file = "predictions.txt"
@@ -53,3 +35,10 @@ with open(output_file, "w", encoding="utf-8") as file:
         file.write(f"{row['ID']} ::: {row['TITLE']} ::: {row['PREDICTED_GENRE']} ::: {row['DESCRIPTION']}\n")
 
 print(f"Predictions saved to {output_file}")
+while True:
+    user_input = input("Enter a movie description (or type 'exit' to quit): ")
+    if user_input.lower() == 'exit':
+        break
+    user_input_tfidf = vectorizer.transform([user_input])
+    predicted_genre = classifier.predict(user_input_tfidf)
+    print(f"Predicted genre: {predicted_genre[0]}")
